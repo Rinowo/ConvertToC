221c4fbd811587c7399aef28b539cf83f9fadc4b
Rinowo <tunganhhh205@gmail.com>
2022-08-22 08:58:47 +0700

    1

--- ConvertC/BinarySearch.cs
+++ ConvertC/BinarySearch.cs
@@ -8,5 +8,37 @@ namespace ConvertC
 {
     internal class BinarySearch
     {
+        static void Main(string[] strings)
+        {
+            int[] arr = new int[10] { 23, 9, 85, 12, 99, 34, 60, 15, 100, 1 };
+            int low = 0, high = 9, mid;
+
+            int giaTri, timThay = 0;
+
+            Console.WriteLine("Nhap 1 gia tri: ");
+            giaTri = Console.Read();
+
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                if (arr[mid] == giaTri)
+                {
+                    Console.WriteLine("Gia tri " + giaTri + "duoc tim thay o vi tri " + mid + " cua mang");
+                    timThay = 1;
+                    break;
+                } if(arr[mid] > giaTri)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            if (timThay == 0)
+            {
+                Console.WriteLine("Gia tri " + giaTri + " khong duoc tim thay trong mang");
+            }
+        }
     }
 }
--- ConvertC/BubbleSort.cs
+++ ConvertC/BubbleSort.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConvertC
 {
-    internal class Class1
+    internal class BubbleSort
     {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10] { 23, 9, 85, 12, 99, 34, 60, 15, 100, 1 };
+            int i, j, tmp, n = 10;
+
+            for(i = 0; i < n; i++)
+            {
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        tmp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = tmp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array after sort: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
     }
 }
